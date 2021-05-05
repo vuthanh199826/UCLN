9a574598628757244e7117d0746f56e395cb348e
vuthanh199826 <vuthanh199825@gmail.com>
2021-05-05 22:10:18 +0700

    hi

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter b: ");
+        int b = sc.nextInt();
+        if (a == 0 && b == 0) {
+            System.out.println("nothing");
+        }
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+        System.out.println(a);
+
+
+    }
+}
